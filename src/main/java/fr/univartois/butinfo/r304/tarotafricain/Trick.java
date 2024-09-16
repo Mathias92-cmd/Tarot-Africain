b2d43df9d57f3fb5bc64ecda963b355bb4acc21d
Mathias92-cmd <mathias_morel@ens.univ-artois.fr>
2024-09-16 10:46:47 +0200

    Add gitignore

--- src/main/java/fr/univartois/butinfo/r304/tarotafricain/Trick.java
+++ src/main/java/fr/univartois/butinfo/r304/tarotafricain/Trick.java
@@ -4,10 +4,14 @@ import fr.univartois.butinfo.r304.tarotafricain.cards.Card;
 
 public class Trick {
     public boolean isEmpty(){
-
+        return false;
     }
 
     public Card getBestCard(){
+
+    }
+
+    public int getWinner() {
 
     }
 }
